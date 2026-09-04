7375da48a23050eaf13cff7826c183fc413102e8
RAJA GAUTAM <rajagautam2021400@gmail.com>
2026-09-04 14:30:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+```javascript
 function openStory() {
+
     document.getElementById("story").scrollIntoView({
         behavior: "smooth"
     });
+
 }
 
+
 function goToMemories() {
+
     document.getElementById("memories").scrollIntoView({
         behavior: "smooth"
     });
+
 }
 
-```javascript
+
 function goToNext() {
 
-    const gallery = document.getElementById("photo-gallery");
-
-    if (gallery) {
-        gallery.scrollIntoView({
-            behavior: "smooth"
-        });
-    }
+    document.getElementById("photo-gallery").scrollIntoView({
+        behavior: "smooth"
+    });
 
 }
-```
 
-```javascript
+
 function goToFinal() {
 
     document.getElementById("photo-gallery").scrollIntoView({
