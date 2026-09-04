95397f19cb36e460fd6134abc53639469c84b0fb
RAJA GAUTAM <rajagautam2021400@gmail.com>
2026-09-04 13:57:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 function openStory() {
-
     document.getElementById("story").scrollIntoView({
         behavior: "smooth"
     });
-
 }
-
 
 function goToMemories() {
-
     document.getElementById("memories").scrollIntoView({
         behavior: "smooth"
     });
-
 }
 
-
 function goToNext() {
-
     document.getElementById("memories").scrollIntoView({
         behavior: "smooth"
     });
-
 }
